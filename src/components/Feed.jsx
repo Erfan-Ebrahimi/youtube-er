6d2cb6faa637f5d4b78683fa16a63c5d6f2a40d0
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-01-08 00:49:10 +0330

    Feed primary developed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useState , useEffect } from "react";
+
+import Sidebar from "./Sidebar";
+
+import { Box , Stack , Typography } from "@mui/material";
+
+
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{flexDirection: {sx:"column" , md:"row"}}}>
+      <Box sx={{height:{sx:"auto" , md:"92vh"}, borderRight:"1px solid #3d3d" , px:{sx:0 , md:2}}}>
+        <Typography className="copyRight" variant="body2" sx={{mt:1.5}}>
+          <Sidebar/>
+          copyRight 2023 erfanMedia
+        </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY:"auto" , height:"90vh" , flex:"2"}}>
+        <Typography variant="h5" fontWeight="bold" mb={2} sx={{color:"#fff"}}>
+          New <span style={{color:"#dede22"}}>videos</span>
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
-export default Feed
+export default Feed;
