408ce20b92fe2e5fa1039a9448361797a64c131e
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-01-07 17:54:54 +0330

    constants added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import { Route , Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 
 // ____________ COMPONENTS _____________ //
-import {Navbar , Feed , SearchFeed , VideoDetail , ChannelDetail} from "./components";
+import {Navbar , Feed , SearchFeed , VideoDetail , ChannelDetail , SearchBar} from "./components";
 
 
 
 function App() {
   return (
-    <Box sx={{backgroundColor : "#000" , color:"red"}}>
+    <Box sx={{backgroundColor : "#000" , color:"white"}}>
       <Navbar/>
       <Routes>
         <Route path="/" exact element={<Feed/>}/>
         <Route path="/video/:id" element={<VideoDetail/>}/>
         <Route path="/channel/:id" element={<ChannelDetail/>}/>
         <Route path="/video/:searchTerm" element={<SearchFeed/>}/>
+        <Route path="/video/:searchTerm" element={<SearchBar/>}/>
       </Routes>
     </Box>
   );
