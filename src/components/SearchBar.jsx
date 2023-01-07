1a51d479619ad676c0cd7688f2ebea1e28606354
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-01-07 17:53:21 +0330

    SearchBar primary developed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Paper , IconButton } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <Paper
+        component="form"
+        onSubmit={() => {}}
+        sx={{borderRadius:20, border: "1px solid red", boxShadow: "none" , pl: 2 , mr:{sm: 5} }}
+    >
+        <input
+            className="search-bar"
+            placeholder="Search . . . "
+            value=""
+            onChange={() => {}}
+        />
+        <IconButton type="submit" sx={{p: "10px" , color: "red"}} >
+            <SearchIcon/>
+        </IconButton>
+    </Paper>
   )
 }
 
