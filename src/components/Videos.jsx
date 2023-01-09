d8a5886dc777889029d988033a9d15b8c8074024
Ebrahimi.Erfan <ebrahimi.erfan89@gmail.com>
2023-01-09 04:37:37 +0330

    Videos : videoCard & channelCard added

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,12 +1,21 @@
+import { Stack , Box } from "@mui/material";
 
+import {VideoCard , ChannelCard} from "./";
 
 
 const Videos = ({videos}) => {
   
-  console.log(videos)
-  
+  // console.log(videos)
+  // dar api ham channel darim ham video pas nemitavanim yek model map bezanim balke ba tavagoh be noe id ke darand tashkhis midahim ke video mibashad ya channel
   return (
-    <div>Videos</div>
+    <Stack direction="row" flexWrap="wrap" justifyContent="flex-start" gap={2} >
+      {videos.map((item , idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item}/>}
+          {item.id.channelId && <ChannelCard channelDetail={item}/>}
+        </Box>
+      ))}
+    </Stack>
   )
 }
 
